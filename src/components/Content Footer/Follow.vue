6c0aaffb0bbfad7f2ada196302c79bf78f807ace
Laurentiu <lcostin40@gmail.com>
2021-11-11 18:34:53 +0100

    Aggiunto follow al footer e terminato esercizio

---
+++ src/components/Content Footer/Follow.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="black">
+    <div class="cont dispi">
+      <button>SIGN-UP NOW!</button>
+      <div class="social flex">
+        <p>FOLLOW US</p>
+        <img src="../../assets/img/footer-facebook.png" alt="" />
+        <img src="../../assets/img/footer-twitter.png" alt="" />
+        <img src="../../assets/img/footer-youtube.png" alt="" />
+        <img src="../../assets/img/footer-pinterest.png" alt="" />
+        <img src="../../assets/img/footer-periscope.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.black {
+  background-color: #303030;
+}
+
+button {
+  padding: 15px 20px;
+  margin: 50px 0;
+  background-color: #303030;
+  border: 2px solid #0282f9;
+  color: #ffffff;
+  font-weight: bold;
+}
+
+.social {
+  align-items: center;
+
+  p {
+    color: #0282f9;
+    font-weight: bold;
+    font-size: 20px;
+    margin-right: 10px;
+  }
+
+  img {
+    margin: 0 10px;
+  }
+}
+</style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,19 @@
 <template>
-  <Links />
+  <footer>
+    <Links />
+    <Follow />
+  </footer>
 </template>
 
 <script lang="ts">
 import Links from "./Content Footer/Links.vue";
+import Follow from "./Content Footer/Follow.vue";
 
 export default {
   name: "App",
   components: {
     Links,
+    Follow,
   },
 };
 </script>
