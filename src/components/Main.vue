e395077d806e56ac625b0f13f43063bd67b87bdb
Laurentiu <lcostin40@gmail.com>
2021-11-12 12:53:19 +0100

    Aggiunto Jumbotron img

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <main>
+    <div class="jumbo"></div>
     <Content />
     <Utility />
   </main>
@@ -22,5 +23,13 @@ export default {
 <style lang="scss">
 main {
   background-color: black;
+}
+
+.jumbo {
+  width: 100%;
+  height: 400px;
+  background-image: url("../assets/img/jumbotron.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
 }
 </style>
